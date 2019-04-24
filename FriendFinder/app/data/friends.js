c6660e62ebf33c5c8558d7a1988cc6a566194dec
irina.montgomery <irina.montgomery@gmail.com>
2019-04-24 15:57:56 -0500

    new

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,11 @@
+var friendArray = [
+  {
+    name: "Irina",
+    age: 26,
+    phoneNumber: "000-000-000",
+    email: "irina@example.com",
+    scores: [1, 0, 0, 1, 1, 0, 0, 1, 1, 1]
+  }
+];
+
+module.exports = friendArray;
---
+++ FriendFinder/server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
